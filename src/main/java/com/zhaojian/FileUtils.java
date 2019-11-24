8ac353990217f4816597778e57f0c77971904336
zhaojian188 <1294626274@qq.com>
2019-11-24 19:44:33 +0800

    完成网址校验的工具类 ==>11-24 19:45

--- src/main/java/com/zhaojian/FileUtils.java
+++ src/main/java/com/zhaojian/FileUtils.java
@@ -30,7 +30,15 @@ import java.util.List;
  * 解析字符的工具类
  */
 public class FileUtils {
-	
+	/**
+	 * 
+	 * @Title: readFile 
+	 * @Description: 解析字符串
+	 * @param fileName
+	 * @return
+	 * @throws IOException
+	 * @return: List<String>
+	 */
 	public static List<String> readFile(String fileName) throws IOException {
 		//创建一个集合存入解析的字符串
 		List<String> lines = new ArrayList<String>();
@@ -44,5 +52,32 @@ public class FileUtils {
 			lines.add(str);
 		}
 		return lines;
+	}
+	
+	/**
+	 * 
+	 * @Title: isHttpUrl 
+	 * @Description:  验证是否为正确的URL
+	 * @param str
+	 * @return
+	 * @return: boolean
+	 */
+	public static boolean isHttpUrl(String str){
+		
+		 //转换为小写
+        str = str.toLowerCase();
+        String regex = "^((https|http|ftp|rtsp|mms)?://)"  //https、http、ftp、rtsp、mms
+                + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" //ftp的user@  
+               + "(([0-9]{1,3}\\.){3}[0-9]{1,3}" // IP形式的URL- 例如：199.194.52.184               
+                 + "|" // 允许IP和DOMAIN（域名） 或单域名
+                 + "[0-9a-z]*"  // 或单域名
+                 + "|" // 允许IP和DOMAIN（域名） 或单域名
+                 + "([0-9a-z_!~*'()-]+\\.)*" // 域名- www.  
+                 + "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\\." // 二级域名  
+                + "[a-z]{2,6})" // first level domain- .com or .museum  
+                + "(:[0-9]{1,5})?" // 端口号最大为65535,5位数
+                + "((/?)|" // a slash isn't required if there is no file name  
+                + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";  
+        return  str.matches(regex);	
 	}
 }
